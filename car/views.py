d04b2370dbc3c5ad589fe543f858080c9796fcde
özkan demir <ozkanndemir2009@gmail.com>
2023-01-26 22:45:30 +0300

    edit

--- car/views.py
+++ car/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Car, Reservation
+from .serializers import CarSerializer
+from .permissions import IsStaffOrReadOnly
+
+
+class CarView(ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = (IsStaffOrReadOnly,)  # [IsStaffOrReadOnly]
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            queryset = super().get_queryset()
+        else:
+            queryset = super().get_queryset().filter(availability=True)
+        start = self.request.query_params.get('start')
+        print(start)
+        end = self.request.query_params.get('end')
+        print(end)
+
+        return queryset
