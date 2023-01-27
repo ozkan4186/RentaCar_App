33c4fbc2b2a1bd807866a76f56d440d3bbb545d3
özkan demir <ozkanndemir2009@gmail.com>
2023-01-27 11:57:18 +0300

    edit

--- car/views.py
+++ car/views.py
@@ -4,6 +4,8 @@ from .models import Car, Reservation
 from .serializers import CarSerializer
 from .permissions import IsStaffOrReadOnly
 
+from django.db.models import Q
+
 
 class CarView(ModelViewSet):
     queryset = Car.objects.all()
@@ -16,8 +18,19 @@ class CarView(ModelViewSet):
         else:
             queryset = super().get_queryset().filter(availability=True)
         start = self.request.query_params.get('start')
-        print(start)
         end = self.request.query_params.get('end')
-        print(end)
+
+        cond1 = Q(start_date__lt=end)
+        cond2 = Q(end_date__gt=start)
+        # not_available = Reservation.objects.filter(
+            # start_date__lt=end, end_date__gt=start
+        # ).values_list('car_id', flat=True)  # [1, 2]
+
+        not_available = Reservation.objects.filter(
+            cond1 & cond2
+        ).values_list('car_id', flat=True)  # [1, 2]
+        print(not_available)
+
+        queryset = queryset.exclude(id__in=not_available)
 
         return queryset
